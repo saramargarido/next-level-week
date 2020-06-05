6138b6a5e2c7b371224842328bea0bd1fe9e13ae
saramargarido <saramargarido@gmail.com>
2020-06-05 19:50:28 -0300

    form, javaScript - UF e cidade

---
+++ scripts/intro.js
@@ -0,0 +1,55 @@
+// document.write("Hello")
+// variaveis
+// tipos de dados
+// myvar = "Hello"
+
+// document.write(myvar)
+
+// const myconst = "he"
+
+// document.write(myconst + myvar)
+
+// objetos possuem propriedades e funcionalidades
+
+// const pessoa = {
+//   altura: "1,80m",
+//   idade: 24,
+//   solteiro: true,
+//   correr(){
+//     return "run forest"
+//   }
+// }
+
+// document.write(pessoa.correr()) 
+// ponto (.) acessa valor
+
+// Array - vetor
+
+// const colecaoBolinhas = ["blue", "green", 2, {name: "my name"}]
+
+// document.write(colecaoBolinhas[3].name)
+
+// registrar função:
+// function sayMyName(name) {
+//   document.write(name)
+// }
+
+// executar:
+// sayMyName("Sara")
+// sayMyName("Clara")
+
+// condicionais:
+
+// const notaFinal = 8
+
+// if (notaFinal < 5) {
+//   document.write("reprovado")
+// } else {
+//   document.write("aprovado")
+// }
+
+//loop - repetições
+
+// for (i = 0; i < 10; i++) {
+//   document.write(`<p> ${i} </p>`)
+// }
